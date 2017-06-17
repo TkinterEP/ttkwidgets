6e5f6c0b3b4ef15e645bdac9812cc7b975235c50
RedFantom <redfantom@outlook.com>
2017-06-17 15:20:35 +0200

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Copyright (c) RedFantom 2017
+# For license see LICENSE
+from setuptools import setup
+
+setup(
+    name="ttkwidgets",
+    packages=["ttkwidgets"],
+    version="0.0.1",
+    description=" A collection of widgets for Tkinter's ttk extensions by various authors ",
+    author="RedFantom and others",
+    url="https://www.github.com/RedFantom/ttkwidgets",
+    download_url="https://www.github.com/RedFantom/ttkwidgets",
+    license="AGPL",
+    classifiers=["Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: GNU Affero General Public License v2 or later (AGPLv2+)"],
+    include_package_data=True
+)
