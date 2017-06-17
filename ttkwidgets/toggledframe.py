71dd17838c16021b4bd9de2c9123319ba1dd4d55
RedFantom <redfantom@outlook.com>
2017-06-17 17:46:51 +0200

    Update to fix toggle function

--- ttkwidgets/toggledframe.py
+++ ttkwidgets/toggledframe.py
@@ -15,13 +15,16 @@ class ToggledFrame(ttk.Frame):
     """
     A frame that can be toggled to open and close
     """
-    def __init__(self, master=None, labeltext="", compound=tk.LEFT, **kwargs):
+
+    def __init__(self, master=None, text="", width=20, compound=tk.LEFT, **kwargs):
         ttk.Frame.__init__(self, master, **kwargs)
-        self.__open = tk.BooleanVar()
+        self._open = False
+        self.__checkbutton_var = tk.BooleanVar()
         self._open_image = ImageTk.PhotoImage(Image.open(os.path.join(get_assets_directory(), "open.png")))
         self._closed_image = ImageTk.PhotoImage(Image.open(os.path.join(get_assets_directory(), "closed.png")))
-        self._checkbutton = ttk.Checkbutton(self, style="Toolbutton", command=self.toggle, variable=self.__open,
-                                            text=labeltext, compound=compound, image=self._closed_image)
+        self._checkbutton = ttk.Checkbutton(self, style="Toolbutton", command=self.toggle,
+                                            variable=self.__checkbutton_var, text=text, compound=compound,
+                                            image=self._closed_image, width=width)
         self.interior = ttk.Frame(self, relief=tk.SUNKEN)
         self._grid_widgets()
 
@@ -29,12 +32,24 @@ class ToggledFrame(ttk.Frame):
         self._checkbutton.grid(row=0, column=0, sticky="we")
 
     def toggle(self):
-        if self.__open.get():
-            self.__open.set(False)
-            self.interior.grid(row=1, column=0, sticky="nswe")
-            self._checkbutton.config(image=self._open_image)
-        else:
-            self.__open.set(True)
+        print(self._open)
+        if self._open:
+            self._open = False
+            self.__checkbutton_var.set(False)
             self.interior.grid_forget()
             self._checkbutton.config(image=self._closed_image)
+        else:
+            self._open = True
+            self.__checkbutton_var.set(True)
+            self.interior.grid(row=1, column=0, sticky="nswe")
+            self._checkbutton.config(image=self._open_image)
 
+
+if __name__ == '__main__':
+    window = tk.Tk()
+    frame = ToggledFrame(window, text="Value", width=10)
+    frame.pack()
+    button = ttk.Button(frame.interior, text="Button", command=window.destroy)
+    button.grid()
+    frame.toggle()
+    window.mainloop()
