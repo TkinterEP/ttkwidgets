cadbca7beb61beea175ed418271e638b2614b1a2
RedFantom <redfantom@outlook.com>
2017-06-17 15:34:37 +0200

    Added basic VerticalScrollFrame test

---
+++ tests/test_verticalscrollframe.py
@@ -0,0 +1,21 @@
+# Copyright (c) RedFantom 2017
+# For license see LICENSE
+from ttkwidgets import VerticalScrollFrame
+import unittest
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+
+
+class TestVerticalScrollFrame(unittest.TestCase):
+    def setUp(self):
+        self.window = tk.Tk()
+
+    def tearDown(self):
+        self.window.destroy()
+
+    def test_vertical_scroll_frame_init(self):
+        frame = VerticalScrollFrame(self.window)
+        frame.pack()
+        self.window.update()
