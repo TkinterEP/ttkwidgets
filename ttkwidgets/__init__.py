1af8b762c11d029b27def99ce4b6c9ec166369cd
RedFantom <redfantom@outlook.com>
2017-06-18 13:57:07 +0200

    Add LinkLabel to __init__.py

--- ttkwidgets/__init__.py
+++ ttkwidgets/__init__.py
@@ -3,3 +3,4 @@
 from ttkwidgets.calendarwidget import Calendar
 from ttkwidgets.verticalscrollframe import VerticalScrollFrame
 from ttkwidgets.toggledframe import ToggledFrame
+from ttkwidgets.linklabel import LinkLabel
