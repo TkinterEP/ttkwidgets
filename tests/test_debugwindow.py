7169e2fa89dc80ef57aa5b271199f768b501b4a0
RedFantom <redfantom@outlook.com>
2017-06-18 21:32:16 +0200

    Add tests for DebugWindow

---
+++ tests/test_debugwindow.py
@@ -0,0 +1,38 @@
+# Copyright (c) RedFantom 2017
+# For license see LICENSE
+from ttkwidgets import DebugWindow
+from tests import BaseWidgetTest
+import mock
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+import os
+import sys
+
+
+def is_python_3():
+    return sys.version_info[0] is 3
+
+
+class TestDebugWindow(BaseWidgetTest):
+    def test_debugwindow_init(self):
+        debug = DebugWindow(self.window)
+        self.window.update()
+
+    def test_debugwindow_print(self):
+        debug = DebugWindow(self.window)
+        print("Something!")
+        self.window.update()
+        self.assertTrue("Something!" in debug.text.get("1.0", tk.END))
+
+    def test_debugwindow_save(self):
+        debug = DebugWindow(self.window)
+        print("Something!")
+        self.window.update()
+        module = "tkinter.filedialog.saveasfilename" if is_python_3() else "tkFileDialog.saveasfilename"
+        with mock.patch(module, "somefile.txt"):
+            debug.save()
+        self.assertTrue(os.path.exists("somefile.txt"))
+        with open("somefile.txt", "r") as f:
+            self.assertTrue("Something!" in f.read())
