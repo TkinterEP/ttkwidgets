edfb9bae41bd9f83b2cf8f33887eadde45a1b2ce
RedFantom <redfantom@outlook.com>
2017-06-18 19:25:30 +0200

    Disable FontFrame tests for Travis-CI

--- tests/test_fontframe.py
+++ tests/test_fontframe.py
@@ -8,78 +8,79 @@ try:
 except ImportError:
     import tkinter as tk
     from tkinter import font
+import os
+
+if "TRAVIS" not in os.environ:
+    class TestFontChooser(BaseWidgetTest):
+        def test_fontchooser_init(self):
+            chooser = FontChooser(self.window)
+            self.window.update()
+
+        def test_fontchooser_noselection(self):
+            chooser = FontChooser(self.window)
+            self.window.update()
+            results = chooser.font
+            self.assertEqual(results, (None, None))
+
+        def test_fontchooser_selection(self):
+            chooser = FontChooser(self.window)
+            self.window.update()
+            chooser._font_family_list.listbox.selection_set(1)
+            chooser._font_family_list._on_click()
+            self.window.update()
+            results = chooser.font
+            self.assertIsInstance(results, tuple)
+            self.assertEqual(len(results), 2)
+            self.assertIsInstance(results[0], tuple)
+            self.assertEqual(len(results[0]), 2)
+            self.assertIsInstance(results[1], font.Font)
+
+        def test_fontchooser_properties(self):
+            chooser = FontChooser(self.window)
+            self.window.update()
+            chooser._font_family_list.listbox.selection_set(1)
+            chooser._font_family_list._on_click()
+            self.window.update()
+            chooser._font_properties_frame._bold.set(True)
+            chooser._font_properties_frame._on_click()
+            self.window.update()
+            results = chooser.font
+            print(results)
+            self.assertTrue("bold" in results[0])
+
+        def test_fontchooser_size(self):
+            chooser = FontChooser(self.window)
+            self.window.update()
+            chooser._font_family_list.listbox.selection_set(1)
+            chooser._font_family_list._on_click()
+            self.window.update()
+            chooser._size_dropdown.delete(0, tk.END)
+            chooser._size_dropdown.insert(0, "14")
+            self.window.update()
+            chooser._size_dropdown._on_click(None)
+            self.window.update()
+            results = chooser.font
+            self.assertEqual(results[0][1], 14)
 
 
-class TestFontChooser(BaseWidgetTest):
-    def test_fontchooser_init(self):
-        chooser = FontChooser(self.window)
-        self.window.update()
+    class TestFontSelectFrame(BaseWidgetTest):
+        def test_fontselectframe_init(self):
+            frame = FontSelectFrame(self.window)
+            frame.pack()
+            self.window.update()
 
-    def test_fontchooser_noselection(self):
-        chooser = FontChooser(self.window)
-        self.window.update()
-        results = chooser.font
-        self.assertEqual(results, (None, None))
-
-    def test_fontchooser_selection(self):
-        chooser = FontChooser(self.window)
-        self.window.update()
-        chooser._font_family_list.listbox.selection_set(1)
-        chooser._font_family_list._on_click()
-        self.window.update()
-        results = chooser.font
-        self.assertIsInstance(results, tuple)
-        self.assertEqual(len(results), 2)
-        self.assertIsInstance(results[0], tuple)
-        self.assertEqual(len(results[0]), 2)
-        self.assertIsInstance(results[1], font.Font)
-
-    def test_fontchooser_properties(self):
-        chooser = FontChooser(self.window)
-        self.window.update()
-        chooser._font_family_list.listbox.selection_set(1)
-        chooser._font_family_list._on_click()
-        self.window.update()
-        chooser._font_properties_frame._bold.set(True)
-        chooser._font_properties_frame._on_click()
-        self.window.update()
-        results = chooser.font
-        print(results)
-        self.assertTrue("bold" in results[0])
-
-    def test_fontchooser_size(self):
-        chooser = FontChooser(self.window)
-        self.window.update()
-        chooser._font_family_list.listbox.selection_set(1)
-        chooser._font_family_list._on_click()
-        self.window.update()
-        chooser._size_dropdown.delete(0, tk.END)
-        chooser._size_dropdown.insert(0, "14")
-        self.window.update()
-        chooser._size_dropdown._on_click(None)
-        self.window.update()
-        results = chooser.font
-        self.assertEqual(results[0][1], 14)
-
-
-class TestFontSelectFrame(BaseWidgetTest):
-    def test_fontselectframe_init(self):
-        frame = FontSelectFrame(self.window)
-        frame.pack()
-        self.window.update()
-
-    def test_fontselectframe_family(self):
-        frame = FontSelectFrame(self.window)
-        frame.pack()
-        self.window.update()
-        frame._family_dropdown.set(font.families()[1])
-        self.window.update()
-        frame._on_family(frame._family_dropdown.get())
-        results = frame.font
-        self.assertIsInstance(results, tuple)
-        self.assertEqual(len(results), 2)
-        self.assertIsInstance(results[0], tuple)
-        self.assertEqual(len(results[0]), 2)
-        self.assertIsInstance(results[1], font.Font)
+        def test_fontselectframe_family(self):
+            frame = FontSelectFrame(self.window)
+            frame.pack()
+            self.window.update()
+            frame._family_dropdown.set(font.families()[1])
+            self.window.update()
+            frame._on_family(frame._family_dropdown.get())
+            results = frame.font
+            self.assertIsInstance(results, tuple)
+            self.assertEqual(len(results), 2)
+            self.assertIsInstance(results[0], tuple)
+            self.assertEqual(len(results[0]), 2)
+            self.assertIsInstance(results[1], font.Font)
 
 
