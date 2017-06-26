7cdc236a75b94a6ac4cf7ef47315fafaa08cafd5
RedFantom <redfantom@outlook.com>
2017-06-26 22:39:03 +0200

    Update DebugWindow with automatic text wrap

--- ttkwidgets/debugwindow.py
+++ ttkwidgets/debugwindow.py
@@ -18,7 +18,8 @@ class DebugWindow(tk.Toplevel):
     """
     A Toplevel that shows sys.stdout and sys.stderr for Tkinter applications
     """
-    def __init__(self, master=None, title="Debug window", stdout=True, stderr=False, **kwargs):
+    def __init__(self, master=None, title="Debug window", stdout=True, stderr=False, width=70, **kwargs):
+        self._width = width
         tk.Toplevel.__init__(self, master, **kwargs)
         self.wm_title(title)
         if stdout:
@@ -31,7 +32,7 @@ class DebugWindow(tk.Toplevel):
         self.filemenu.add_command(label="Save file", command=self.save)
         self.filemenu.add_command(label="Exit", command=self.destroy)
         self.menu.add_cascade(label="File", menu=self.filemenu)
-        self.text = tk.Text(self)
+        self.text = tk.Text(self, width=width)
         self.scroll = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.text.yview)
         self.text.config(yscrollcommand=self.scroll.set)
         self.text.bind("<Key>", lambda e: "break")
@@ -49,7 +50,14 @@ class DebugWindow(tk.Toplevel):
         self.scroll.grid(row=0, column=1, sticky="ns")
 
     def write(self, line):
-        self.text.insert(tk.END, line)
+        while True:
+            if len(line) <= self._width:
+                self.text.insert(tk.END, line)
+                break
+            else:
+                sub = line[:self._width]
+                self.text.insert(tk.END, sub)
+                self.text.insert(tk.END, "\n")
 
 
 if __name__ == '__main__':
