31ba4595c3f57744b90fe28491311bd3c130375e
RedFantom <redfantom@outlook.com>
2017-07-12 16:05:23 +0200

    Move scrolledframe.py and toggledframe.py for #8

--- ttkwidgets/__init__.py
+++ ttkwidgets/__init__.py
@@ -1,8 +1,6 @@
 # Copyright (c) The ttkwidgets authors 2017
 # Available under the license found in LICENSE
 from ttkwidgets.calendarwidget import Calendar
-from ttkwidgets.scrolledframe import ScrolledFrame
-from ttkwidgets.toggledframe import ToggledFrame
 from ttkwidgets.linklabel import LinkLabel
 from ttkwidgets.scrolledlistbox import ScrolledListbox
 from ttkwidgets.autocompletewidgets import AutocompleteCombobox, AutocompleteEntry
---
+++ ttkwidgets/frames/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) The ttkwidgets authors 2017
+# Available under the license found in LICENSE
+from .scrolledframe import ScrolledFrame
+from .toggledframe import ToggledFrame
