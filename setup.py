cabc9dc0f4e42528e28135a21e7cd7de0be3adba
RedFantom <redfantom@outlook.com>
2017-08-13 22:10:33 +0200

    Update setup call as suggested by @j4321

--- setup.py
+++ setup.py
@@ -4,15 +4,9 @@ from setuptools import setup
 
 setup(
     name="ttkwidgets",
-    packages=["ttkwidgets"],
+    packages=["ttkwidgets", "ttkwidgets.frames", "ttkwidgets.font", "ttkwidgets.autocomplete", "ttkwidgets.color"],
     py_modules=["ttkwidgets"],
-    package_data={
-        "ttkwidgets": ["assets/*"],
-        "ttkwidgets.autocomplete": ["*.py"],
-        "ttkwidgets.color": ["*.py"],
-        "ttkwidgets.font": ["*.py"],
-        "ttkwidgets.frames": ["*.py"]
-    },
+    package_data={"ttkwidgets": ["assets/*"]},
     version="0.6.0",
     description=" A collection of widgets for Tkinter's ttk extensions by various authors ",
     author="RedFantom and others",
@@ -22,7 +16,5 @@ setup(
     classifiers=["Programming Language :: Python :: 2.7",
                  "Programming Language :: Python :: 3",
                  "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
-    install_requires=["pillow"],
-    zip_safe=False
+    install_requires=["pillow"]
 )
-
