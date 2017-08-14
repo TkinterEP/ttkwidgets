b63df7b4c4eb04f956180d010024e858bcffe59f
RedFantom <redfantom@outlook.com>
2017-08-14 13:03:32 +0200

    Add docstrings

--- ttkwidgets/balloon.py
+++ ttkwidgets/balloon.py
@@ -21,6 +21,15 @@ class Balloon(ttk.Frame):
 
     def __init__(self, master=None, headertext="Help", text="Some great help is displayed here.", width=200, timeout=1,
                  background="#fef9cd", **kwargs):
+        """
+        :param master: widget to bind to
+        :param headertext: text to show in window header
+        :param text: text to show as help text
+        :param width: width of the window
+        :param timeout: timeout in seconds (can be float) to wait until Balloon is shown
+        :param background: background color of the help Balloon
+        :param kwargs: keyword arguments for Frame creation
+        """
         if background is "white":
             raise ValueError("White is the only background color not currently allowed.")
         ttk.Frame.__init__(self, master, **kwargs)
@@ -45,19 +54,32 @@ class Balloon(ttk.Frame):
         self.master.bind("<Leave>", self._on_leave)
 
     def _grid_widgets(self):
+        """
+        Place the widgets in the Toplevel
+        """
         self._canvas.grid(sticky="nswe")
         self.header_label.grid(row=1, column=1, sticky="nswe", pady=5, padx=5)
         self.text_label.grid(row=3, column=1, sticky="nswe", pady=6, padx=5)
 
     def _on_enter(self, event):
+        """
+        Creates a delayed callback for the <Enter> event
+        """
         self._id = self.master.after(self._timeout * 1000, func=self.show)
 
     def _on_leave(self, event):
+        """
+        Callback for the <Leave> event to destroy the Toplevel
+        """
         if self._toplevel:
             self._toplevel.destroy()
         self.master.after_cancel(self._id)
 
     def show(self):
+        """
+        Callback for the delayed <Enter> event (see _on_enter) to create the Toplevel widget and its child widgets to
+        show in the spot of the cursor.
+        """
         self._toplevel = tk.Toplevel(self.master)
         self._canvas = tk.Canvas(self._toplevel, background=self.__background)
         self.header_label = ttk.Label(self._canvas, text=self.__headertext, background=self.__background,
@@ -69,8 +91,11 @@ class Balloon(ttk.Frame):
         self._grid_widgets()
         x, y = self.master.winfo_pointerxy()
         self._canvas.update()
+        # Update the Geometry of the Toplevel to update its position and size
         self._toplevel.geometry("{0}x{1}+{2}+{3}".format(self._canvas.winfo_width(), self._canvas.winfo_height(),
                                                          x + 2, y + 2))
+
+    # TODO: Implement custom cget and config functions
 
 
 if __name__ == '__main__':
