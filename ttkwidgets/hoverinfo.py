f590f1a339892fe368def702b2424e3748a81534
RedFantom <redfantom@outlook.com>
2017-08-14 12:57:40 +0200

    Fix Balloon widget

--- ttkwidgets/hoverinfo.py
+++ ttkwidgets/hoverinfo.py
@@ -14,7 +14,7 @@ import os
 from ttkwidgets.utilities import get_assets_directory
 
 
-class Balloon(tk.Toplevel):
+class Balloon(ttk.Frame):
     """
     Simple help hover balloon
     """
@@ -23,54 +23,54 @@ class Balloon(tk.Toplevel):
                  background="#fef9cd", **kwargs):
         if background is "white":
             raise ValueError("White is the only background color not currently allowed.")
-        tk.Toplevel.__init__(self, master, background="white", **kwargs)
-        self._canvas = tk.Canvas(self, background=background)
-        self.wm_attributes("-transparentcolor", "white")
+        ttk.Frame.__init__(self, master, **kwargs)
+        self._toplevel = None
+        self._canvas = None
+        self.header_label = None
+        self.text_label = None
+
         # The image was found here:
         # https://www.iconfinder.com/icons/26486/balloon_help_information_icon#size=16
         # Under CC Attribution License
         self._image = Image.open(os.path.join(get_assets_directory(), "balloon.png"))
         self._photo_image = ImageTk.PhotoImage(self._image)
         self.__background = background
-        self.header_label = ttk.Label(self._canvas, text=headertext, background=background, image=self._photo_image,
-                                      compound=tk.LEFT)
-        self.text_label = ttk.Label(self._canvas, text=text, wraplength=width, background=background)
-        self.overrideredirect(True)
+        self.__headertext = headertext
+        self.__text = text
+        self.__width = width
         self.master = master
         self._id = None
         self._timeout = timeout
         self.master.bind("<Enter>", self._on_enter)
         self.master.bind("<Leave>", self._on_leave)
-        self.withdraw()
 
     def _grid_widgets(self):
         self._canvas.grid(sticky="nswe")
         self.header_label.grid(row=1, column=1, sticky="nswe", pady=5, padx=5)
         self.text_label.grid(row=3, column=1, sticky="nswe", pady=6, padx=5)
 
-    def _ungrid_widgets(self):
-        self._canvas.grid_forget()
-        self.header_label.grid_forget()
-        self.text_label.grid_forget()
-
     def _on_enter(self, event):
-        print("Entered")
-        self._id = self.after(self._timeout * 1000, func=self.show)
-        print(self._id)
+        self._id = self.master.after(self._timeout * 1000, func=self.show)
 
     def _on_leave(self, event):
-        self.attributes("-topmost", False)
-        print("Left")
-        self.after_cancel(self._id)
-        self._ungrid_widgets()
+        if self._toplevel:
+            self._toplevel.destroy()
+        self.master.after_cancel(self._id)
 
     def show(self):
-        self.attributes("-topmost", True)
+        self._toplevel = tk.Toplevel(self.master)
+        self._canvas = tk.Canvas(self._toplevel, background=self.__background)
+        self.header_label = ttk.Label(self._canvas, text=self.__headertext, background=self.__background,
+                                      image=self._photo_image, compound=tk.LEFT)
+        self.text_label = ttk.Label(self._canvas, text=self.__text, wraplength=self.__width,
+                                    background=self.__background)
+        self._toplevel.attributes("-topmost", True)
+        self._toplevel.overrideredirect(True)
         self._grid_widgets()
-        print("Showing")
-        self.deiconify()
-        x, y = self.winfo_pointerxy()
-        self.geometry("{0}x{1}+{2}+{3}".format(self.winfo_reqwidth(), self.winfo_reqheight(), x + 2, y + 2))
+        x, y = self.master.winfo_pointerxy()
+        self._canvas.update()
+        self._toplevel.geometry("{0}x{1}+{2}+{3}".format(self._canvas.winfo_width(), self._canvas.winfo_height(),
+                                                         x + 2, y + 2))
 
 
 if __name__ == '__main__':
