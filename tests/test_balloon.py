590ea2cd7fd0d9fd60f635bc8b79540b943489f8
RedFantom <redfantom@outlook.com>
2017-08-14 15:10:07 +0200

    Update tests to improve line coverage

--- tests/test_balloon.py
+++ tests/test_balloon.py
@@ -2,11 +2,11 @@
 # For license see LICENSE
 from ttkwidgets.frames import Balloon
 from tests import BaseWidgetTest
-
 try:
     import Tkinter as tk
 except ImportError:
     import tkinter as tk
+from time import sleep
 
 
 class TestBalloon(BaseWidgetTest):
@@ -31,12 +31,35 @@ class TestBalloon(BaseWidgetTest):
         self.assertEqual(balloon.cget("timeout"), 3)
         self.assertEqual(balloon.cget("background"), "black")
 
+        # Keys for the Frame widget
+        balloon.configure(height=40)
+        self.assertEqual(balloon.cget("height"), 40)
+
     def test_balloon_show(self):
         balloon = Balloon(self.window)
         self.window.update()
         balloon.show()
         self.window.update()
+        balloon.config(text="Something else")
+        self.window.update()
+        self.assertEqual(balloon.cget("text"), "Something else")
         balloon._on_leave(None)
         self.window.update()
         self.assertIs(balloon._toplevel, None)
+
+    def test_balloon_events(self):
+        balloon = Balloon(self.window, timeout=0.2)
+        balloon._on_enter(None)
+        self.window.update()
+        sleep(0.3)
+        self.window.update()
+        self.assertIsInstance(balloon._toplevel, tk.Toplevel)
+        balloon._on_leave(None)
+        self.assertIs(balloon._toplevel, None)
+
+    def test_balloon_events_noshow(self):
+        balloon = Balloon(self.window)
+        balloon._on_enter(None)
+        balloon._on_leave(None)
+
 
