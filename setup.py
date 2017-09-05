3c540e385d1dea6e82a41fdec0ab74765fb8f6d3
RedFantom <redfantom@outlook.com>
2017-09-05 15:36:01 +0200

    Merge install into master to fix the installation of ttkwidgets with PR #10
    * Fix the installation of the widget folders
    * Remove deprecated MANIFEST.in file
    * Make package_data argument more consistent
    * Remove include_package_data=True for Unix
    * Update setup call as suggested by @j4321
    * Update author keyword argument

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ from setuptools import setup
 
 setup(
     name="ttkwidgets",
-    packages=["ttkwidgets"],
+    packages=["ttkwidgets", "ttkwidgets.frames", "ttkwidgets.font", "ttkwidgets.autocomplete", "ttkwidgets.color"],
     py_modules=["ttkwidgets"],
     package_data={"ttkwidgets": ["assets/*"]},
     version="0.6.0",
     description=" A collection of widgets for Tkinter's ttk extensions by various authors ",
-    author="RedFantom and others",
+    author="The ttkwidgets authors",
     url="https://www.github.com/RedFantom/ttkwidgets",
     download_url="https://www.github.com/RedFantom/ttkwidgets/releases",
     license="AGPL",
     classifiers=["Programming Language :: Python :: 2.7",
                  "Programming Language :: Python :: 3",
                  "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
-    include_package_data=True,
     install_requires=["pillow"]
 )
-
