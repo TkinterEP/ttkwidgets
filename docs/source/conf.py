9ffc164f4e89c86897cb1d9ee45048e884974b77
Juliette Monsel <j_4321@protonmail.com>
2018-09-28 17:15:27 +0200

    Fix sys.path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 # import os
 import sys
-sys.path.insert(0, '/home/juliette/Documents/ttkwidgets')
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
