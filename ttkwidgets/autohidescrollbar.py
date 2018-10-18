7d97ecd028e19b02768a81b3f3057804511d0602
Juliette Monsel <j_4321@protonmail.com>
2018-10-18 23:45:00 +0200

    Fix AutoHideScrollbar docstrings

--- ttkwidgets/autohidescrollbar.py
+++ ttkwidgets/autohidescrollbar.py
@@ -13,11 +13,12 @@ except ImportError:
 
 class AutoHideScrollbar(ttk.Scrollbar):
     """Scrollbar that automatically hides when not needed."""
-    def __init__(self, *args, **kwargs):
+    def __init__(self, master=None, **kwargs):
         """
         Create the scrollbar.
 
-        Take the same arguments as ttk.Scrollbar
+        :param master: master widget
+        :param kwargs: options to be passed on to the ``ttk.Scrollbar`` initializer
         """
         ttk.Scrollbar.__init__(self, *args, **kwargs)
         self._pack_kw = {}
@@ -25,6 +26,14 @@ class AutoHideScrollbar(ttk.Scrollbar):
         self._layout = 'place'
 
     def set(self, lo, hi):
+        """
+        Set the fractional values of the slider position.
+        
+        :param lo: lower end of the scrollbar (between 0 and 1)
+        :type lo: float
+        :param hi: upper end of the scrollbar (between 0 and 1)
+        :type hi: float
+        """
         if float(lo) <= 0.0 and float(hi) >= 1.0:
             if self._layout == 'place':
                 self.place_forget()
@@ -52,6 +61,39 @@ class AutoHideScrollbar(ttk.Scrollbar):
         return dic
 
     def place(self, **kw):
+        """
+        Place a widget in the parent widget.
+        
+        :param in\_: master relative to which the widget is placed
+        :type in\_: widget
+        :param x: locate anchor of this widget at position x of master
+        :type x: int
+        :param y: locate anchor of this widget at positiony of master
+        :type y: int
+        :param relx: locate anchor of this widget between 0 and 1
+                      relative to width of master (1 is right edge)
+        :type relx: float
+        :param rely: locate anchor of this widget between 0 and 1
+                      relative to height of master (1 is bottom edge)
+        :type rely: float
+        :param anchor: position anchor according to given direction 
+                        ("n", "s", "e", "w" or combinations)
+        :type anchor: str
+        :param width: width of this widget in pixel
+        :type width: int
+        :param height: height of this widget in pixel
+        :type height: int
+        :param relwidth: width of this widget between 0.0 and 1.0
+                          relative to width of master (1.0 is the same width
+                          as the master)
+        :type relwidth: float
+        :param relheight: height of this widget between 0.0 and 1.0
+                           relative to height of master (1.0 is the same
+                           height as the master)
+        :type relheight: float
+        :param bordermode: "inside" or "outside": whether to take border width of master widget into account
+        :type bordermode: str
+        """
         ttk.Scrollbar.place(self, **kw)
         try:
             self._place_kw = self.place_info()
@@ -61,6 +103,33 @@ class AutoHideScrollbar(ttk.Scrollbar):
         self._layout = 'place'
 
     def pack(self, **kw):
+        """
+        Pack a widget in the parent widget.
+        
+        :param after: pack it after you have packed widget
+        :type after: widget
+        :param anchor: position anchor according to given direction 
+                        ("n", "s", "e", "w" or combinations)
+        :type anchor: str
+        :param before: pack it before you will pack widget
+        :type before: widget
+        :param expand: expand widget if parent size grows
+        :type expand: bool
+        :param fill: "none" or "x" or "y" or "both": fill widget if widget grows
+        :type fill: str
+        :param in\_: widget to use as container
+        :type in\_: widget
+        :param ipadx: add internal padding in x direction
+        :type ipadx: int
+        :param ipady: add internal padding in y direction
+        :type ipady: int
+        :param padx: add padding in x direction
+        :type padx: int
+        :param pady: add padding in y irection
+        :type pady: int
+        :param side: "top" (default), "bottom", "left" or "right": where to add this widget
+        :type side: str
+        """
         ttk.Scrollbar.pack(self, **kw)
         try:
             self._pack_kw = self.pack_info()
@@ -70,5 +139,31 @@ class AutoHideScrollbar(ttk.Scrollbar):
         self._layout = 'pack'
 
     def grid(self, **kw):
+        """
+        Position a widget in the parent widget in a grid. 
+        
+        :param column: use cell identified with given column (starting with 0)
+        :type column: int
+        :param columnspan: this widget will span several columns
+        :type columnspan: int
+        :param in\_: widget to use as container
+        :type in\_: widget
+        :param ipadx: add internal padding in x direction
+        :type ipadx: int
+        :param ipady: add internal padding in y direction
+        :type ipady: int
+        :param padx: add padding in x direction
+        :type padx: int
+        :param pady: add padding in y irection
+        :type pady: int
+        :param row: use cell identified with given row (starting with 0)
+        :type row: int
+        :param rowspan: this widget will span several rows
+        :type rowspan: int
+        :param sticky: "n", "s", "e", "w" or combinations: if cell is 
+                       larger on which sides will this widget stick to 
+                       the cell boundary
+        :type sticky: str
+        """
         ttk.Scrollbar.grid(self, **kw)
         self._layout = 'grid'
