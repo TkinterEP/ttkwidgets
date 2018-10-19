7b1eb807919728c232acad0f81820681a8756d59
Juliette Monsel <j_4321@protonmail.com>
2018-10-19 13:33:18 +0200

    Fix Table docstrings

--- ttkwidgets/table.py
+++ ttkwidgets/table.py
@@ -24,7 +24,7 @@ class Table(ttk.Treeview):
     Table widget displays a table with options to drag rows and columns and
     to sort columns.
 
-    This widget is based on the ttk.Treeview and shares many options and methods
+    This widget is based on the :class:`ttk.Treeview` and shares many options and methods
     with it.
     """
     _initialized = False  # to kwnow whether class bindings and Table layout have been created yet
@@ -32,19 +32,22 @@ class Table(ttk.Treeview):
     def __init__(self, master=None, show='headings', drag_cols=True, drag_rows=True,
                  sortable=True, class_='Table', **kwargs):
         """
-        Construct a Table with parent master.
+        Create a Table.
 
-        KEYWORDS OPTIONS
-
-            master: parent window
-            drag_cols: boolean to set whether columns are draggable
-            drag_rows: boolean to set whether rows are draggable
-            sortable: boolean to set whether columns are sortable by clicking on
-                      their headings. The sorting order depends on the type of
-                      data (str, float, ...) which can be set with the column method.
-            show: which parts of the treeview to show (same as the Treeview option)
-            **kwargs: all ttk.Treeview options
-
+        :param master: master widget
+        :type master: widget
+        :param drag_cols: whether columns are draggable
+        :type drag_cols: bool
+        :param drag_rows: whether rows are draggable
+        :type drag_rows: bool
+        :param sortable: whether columns are sortable by clicking on their
+                         headings. The sorting order depends on the type of
+                         data (str, float, ...) which can be set with the column
+                         method.
+        :type sortable: bool
+        :param show: which parts of the treeview to show (same as the Treeview option)
+        :type show: str
+        :param kwargs: options to be passed on to the :class:`ttk.Treeview` initializer
         """
         ttk.Treeview.__init__(self, master, show=show, **kwargs)
         # copy of the Treeview to show the dragged column/row
@@ -353,6 +356,14 @@ class Table(ttk.Treeview):
         return displayed_cols
 
     def cget(self, key):
+        """
+        Query widget option.
+
+        To get the list of options for this widget, call the method :meth:`~Table.keys`.
+
+        :param key: option name
+        :type key: str
+        """
         if key == 'sortable':
             return self._sortable
         elif key == 'drag_cols':
@@ -366,11 +377,22 @@ class Table(ttk.Treeview):
         """
         Query or modify the options for the specified column.
 
-        If kw is not given, returns a dict of the column option values. If
-        option is specified then the value for that option is returned.
+        If `kw` is not given, returns a dict of the column option values. If
+        `option` is specified then the value for that option is returned.
         Otherwise, sets the options to the corresponding values.
 
-        options: 'type' (content type for sorting, default=str), all Treeview column options
+        :param id: the column's identifier (read-only option)
+        :param anchor: "n", "ne", "e", "se", "s", "sw", "w", "nw", or "center":
+                       alignment of the text in this column with respect to the cell
+        :param minwidth: minimum width of the column in pixels
+        :type minwidth: int
+        :param stretch: whether the column's width should be adjusted when the widget is resized
+        :type stretch: bool
+        :param width: width of the column in pixels
+        :type width: int
+        :param type: column's content type (for sorting), default type is `str`
+        :type type: type
+
         """
         config = False
         if option == 'type':
@@ -388,7 +410,12 @@ class Table(ttk.Treeview):
             return res
 
     def configure(self, cnf=None, **kw):
-        """Configure widget options"""
+        """
+        Configure resources of a widget.
+
+        To get the list of options for this widget, call the method :meth:`~Table.keys`.
+        See :meth:`~Table.__init__` for a description of the widget specific option.
+        """
         if cnf == 'drag_cols':
             return 'drag_cols', self._drag_cols
         elif cnf == 'drag_rows':
@@ -453,14 +480,47 @@ class Table(ttk.Treeview):
         self.update_idletasks()
 
     def delete(self, *items):
+        """
+        Delete all specified items and all their descendants. The root item may not be deleted.
+
+        :param items: list of item identifiers
+        :type items: sequence[str]
+        """
         self._visual_drag.delete(*items)
         ttk.Treeview.delete(self, *items)
 
     def detach(self, *items):
+        """
+        Unlinks all of the specified items from the tree.
+
+        The items and all of their descendants are still present, and may be
+        reinserted at another point in the tree, but will not be displayed.
+        The root item may not be detached.
+
+        :param items: list of item identifiers
+        :type items: sequence[str]
+        """
         self._visual_drag.detach(*items)
         ttk.Treeview.detach(self, *items)
 
     def heading(self, column, option=None, **kw):
+        """
+        Query or modify the heading options for the specified column.
+
+        If `kw` is not given, returns a dict of the heading option values. If
+        `option` is specified then the value for that option is returned.
+        Otherwise, sets the options to the corresponding values.
+
+        :param text: text to display in the column heading
+        :type text: str
+        :param image: image to display to the right of the column heading
+        :type image: PhotoImage
+        :param anchor: "n", "ne", "e", "se", "s", "sw", "w", "nw", or "center":
+                       alignement of the heading text
+        :type anchor: str
+        :param command: callback to be invoked when the heading label is pressed.
+        :type command: function
+        """
         if kw:
             # Set the default image of the heading to the drag icon
             kw.setdefault("image", self._im_drag)
@@ -468,10 +528,42 @@ class Table(ttk.Treeview):
         return ttk.Treeview.heading(self, column, option, **kw)
 
     def insert(self, parent, index, iid=None, **kw):
+        """
+        Creates a new item and return the item identifier of the newly created item.
+        
+        :param parent: identifier of the parent item
+        :type parent: str
+        :param index: where in the list of parent's children to insert the new item
+        :type index: int or "end"
+        :param iid: item identifier, iid must not already exist in the tree. If iid is None a new unique identifier is generated.
+        :type iid: None or str
+        :param kw: item's options: see :meth:`~Table.item`
+        
+        :return: the item identifier of the newly created item
+        :rtype: str
+        """
         self._visual_drag.insert(parent, index, iid, **kw)
         return ttk.Treeview.insert(self, parent, index, iid, **kw)
 
     def item(self, item, option=None, **kw):
+        """
+        Query or modify the options for the specified item.
+
+        If no options are given, a dict with options/values for the item is returned.
+        If option is specified then the value for that option is returned.
+        Otherwise, sets the options to the corresponding values as given by `kw`.
+        
+        :param text: item's label
+        :type text: str
+        :param image: image to be displayed on the left of the item's label
+        :type image: PhotoImage
+        :param values: values to put in the columns
+        :type values: sequence
+        :param open: whether the item's children should be displayed
+        :type open: bool
+        :param tags: list of tags associated with this item
+        :type tags: sequence[str]
+        """
         if kw:
             self._visual_drag.item(item, option, **kw)
         return ttk.Treeview.item(self, item, option, **kw)
@@ -481,18 +573,54 @@ class Table(ttk.Treeview):
         return keys + ['sortable', 'drag_cols']
 
     def move(self, item, parent, index):
+        """
+        Moves item to position index in parent’s list of children.
+
+        It is illegal to move an item under one of its descendants. If index is
+        less than or equal to zero, item is moved to the beginning, if greater
+        than or equal to the number of children, it is moved to the end.
+        If item was detached it is reattached.
+
+        :param item: item's identifier
+        :type item: str
+        :param parent: new parent of item
+        :type parent: str
+        :param index: where in the list of parent’s children to insert item
+        :type index: int of "end"
+        """
         self._visual_drag.move(item, parent, index)
         ttk.Treeview.move(self, item, parent, index)
 
-    def reattach(self, item, parent, index):
-        self._visual_drag.reattach(item, parent, index)
-        ttk.Treeview.reattach(self, item, parent, index)
+    reattach = move
 
     def set(self, item, column=None, value=None):
+        """
+        Query or set the value of given item.
+
+        With one argument, return a dictionary of column/value pairs for the
+        specified item. With two arguments, return the current value of the
+        specified column. With three arguments, set the value of given column
+        in given item to the specified value.
+
+        :param item: item's identifier
+        :type item: str
+        :param column: column's identifier
+        :type column: str, int or None
+        :param value: new value
+        """
         if value is not None:
             self._visual_drag.set(item, ttk.Treeview.column(self, column, 'id'), value)
         return ttk.Treeview.set(self, item, column, value)
 
     def set_children(self, item, *newchildren):
+        """
+        Replaces item’s children with newchildren.
+
+        Children present in item that are not present in newchildren are detached
+        from tree. No items in newchildren may be an ancestor of item.
+
+        :param newchildren: new item's children (list of item identifiers)
+        :type newchildren: sequence[str]
+        """
         self._visual_drag.set_children(item, *newchildren)
         ttk.Treeview.set_children(self, item, *newchildren)
