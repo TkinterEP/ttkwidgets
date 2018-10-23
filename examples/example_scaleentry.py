cfa42731d9f7415763b758550025f5cee29f461b
Juliette Monsel <j_4321@protonmail.com>
2018-10-23 09:46:04 +0200

    Add example for ScaleEntry

---
+++ examples/example_scaleentry.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Juliette Monsel 2018
+# For license see LICENSE
+
+from ttkwidgets import ScaleEntry
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+
+
+window = tk.Tk()
+scaleentry = ScaleEntry(window, scalewidth=200, entrywidth=3, from_=0, to=20)
+scaleentry.config_entry(justify='center')
+scaleentry.pack()
+window.mainloop()
