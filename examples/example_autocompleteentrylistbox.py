d841d662326c596ff3f96cb3bbfea4e071534fcf
Juliette Monsel <j_4321@protonmail.com>
2019-08-30 14:11:57 +0200

    Add AutocompleteEntryListbox widget

---
+++ examples/example_autocompleteentrylistbox.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Juliette Monsel 2019
+# For license see LICENSE
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+from ttkwidgets.autocomplete import AutocompleteEntryListbox
+
+window = tk.Tk()
+tk.Label(window, text="Entry + Listbox with autocompletion for the Tk instance's methods:").pack()
+entry = AutocompleteEntryListbox(window, width=20, completevalues=dir(window))
+entry.pack()
+window.mainloop()
