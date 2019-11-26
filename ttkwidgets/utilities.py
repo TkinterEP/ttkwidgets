a92c7062ce2767f1a00e0fb4a9e37dc73059acbe
Dogeek <Dogeek@users.noreply.github.com>
2019-11-26 20:42:19 +0100

    moved Balloon class, added util get_average_character_width

--- ttkwidgets/utilities.py
+++ ttkwidgets/utilities.py
@@ -1,6 +1,8 @@
 # Copyright (c) RedFantom 2017
 import os
 from PIL import Image, ImageTk
+import string
+from tkinter.font import Font
 
 
 def get_assets_directory():
@@ -9,3 +11,28 @@ def get_assets_directory():
 
 def open_icon(icon_name):
     return ImageTk.PhotoImage(Image.open(os.path.join(get_assets_directory(), icon_name)))
+
+
+def get_average_character_width(font):
+    """
+    Gets the average character width for a given tkinter font
+    
+    :param font: the font to use to measure average character width for.
+    :type font: tkinter.font.Font, tuple
+    
+    :returns: average character width for the given font
+    :rtype: float
+    :raises: TypeError
+    """
+    
+    def _avg(list_):
+        return sum(list_) / len(list_)
+    
+    if isinstance(font, (tuple, list)):
+        font = Font(font=tuple(font))
+    elif not isinstance(font, Font):
+        raise TypeError("font argument of function "
+                        "'get_average_character_width' in "
+                        "ttkwidgets.utilities is of type {} "
+                        "instead of (tuple, tkinter.font.Font)".format(str(type(font))))
+    return _avg([font.measure(c) for c in string.printable])
