154ba6d54785920476af36eb04f58b223135f9a4
RedFantom <redfantom@outlook.com>
2019-11-27 13:35:09 +0100

    Update hooks tests
    The tests were actually wrong: Because only a single self.expected variable is used and a single session, basic_updater will get calls from the other hooks (so it may be called with a hook1 option while it is doing the test_user_hook)

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -13,21 +13,37 @@ except ImportError:
 from ttkwidgets.hook import hook_ttk_widgets, generate_hook_name, is_hooked
 
 
+def printf(*args, **kwargs):
+    kwargs["flush"] = True
+    print(*args, **kwargs)
+    
+
 class TestHooks(TestCase):
 
     def setUp(self):
+        self.window = tk.Tk()
         self.expected = {None: None}
         self.updated = False
         self.user_hook_called = False
         self.second_updated = False
+        self.second_expected = {None: None}
 
     def basic_updater(self, widget, option, value):
-        self.assertTrue(option in self.expected)
-        self.assertEquals(value, self.expected[option])
+        if option not in self.expected:
+            print("basic_updater is expecting {} and got {}: {}, so it is ignored".format(
+                self.expected, option, value))
+            return
+        printf("basic_updater is expecting {} and got {}: {}".format(self.expected, option, value))
+        self.assertEquals(value, self.expected[option], "Invalid value for {}: {}".format(option, value))
         self.updated = True
+        self.expected.clear()
 
     def second_updater(self, widget, option, value):
+        if option not in self.second_expected:
+            return  # Not updated with desired option
+        self.assertEquals(value, self.second_expected[option])
         self.second_updated = True
+        self.second_expected.clear()
 
     def has_been_updated(self):
         updated = self.updated
@@ -40,7 +56,9 @@ class TestHooks(TestCase):
         return updated
 
     def test_basic_hook(self):
+        printf("Started test_basic_hook")
         self.expected = {"tooltip": "Hello World"}
+        printf("test_basic_hook is expecting:", self.expected)
         options = {"tooltip": "Default Value"}
         hook_ttk_widgets(self.basic_updater, options)
         ttk.Button(tooltip="Hello World")
@@ -49,8 +67,12 @@ class TestHooks(TestCase):
         self.assertTrue(is_hooked(options))
         self.assertTrue(hasattr(ttk.Button, generate_hook_name(options)))
 
+        printf("Finished test_basic_hook")
+
     def test_user_hook_and_defaults(self):
+        printf("Started test_user_hook", flush=True)
         self.expected = {"not_user": "Hello World"}
+        options = self.expected.copy()
         hook_ttk_widgets(self.basic_updater, self.expected.copy())
 
         button_init = ttk.Button.__init__
@@ -61,15 +83,20 @@ class TestHooks(TestCase):
 
         ttk.Button.__init__ = __init__
 
+        printf("test_user_hook is expecting: ", self.expected)
         ttk.Button()
         self.assertTrue(self.user_hook_called)
-        self.assertTrue(is_hooked(self.expected))
+        self.assertTrue(is_hooked(options))
         self.assertTrue(self.has_been_updated())
+        printf("Finished test_user_hook")
 
     def test_multi_hooks(self):
+        printf("Started test_multi_hooks")
         options1 = {"hook1": "Default One"}
         options2 = {"hook2": "Default Two"}
         self.expected = {"hook1": "Custom One"}
+        printf("test_multi_hooks is expecting: ", self.expected)
+        self.second_expected = {"hook2": "Default Two"}
 
         name = hook_ttk_widgets(self.basic_updater, options1)
         hook_ttk_widgets(self.second_updater, options2)
@@ -85,4 +112,8 @@ class TestHooks(TestCase):
 
         self.assertTrue(self.has_been_updated())
         self.assertTrue(self.has_been_second_updated())
+        printf("Finished test_multi_hooks")
+
+    def tearDown(self):
+        self.window.destroy()
 
