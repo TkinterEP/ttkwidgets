eded02615001216b497f4f8166e20a5d53956a8f
Dogeek <Dogeek@users.noreply.github.com>
2019-11-29 22:58:05 +0100

    document placeholder entry

--- ttkwidgets/entries/placeholderentry.py
+++ ttkwidgets/entries/placeholderentry.py
@@ -9,28 +9,39 @@ import tkinter.ttk as ttk
 
 
 class PlaceholderEntry(ttk.Entry):
+    """ An Entry that takes an argument for a placeholder that'll be put in, and disappear once the widget takes focus"""
     def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey', **kwargs):
+        """
+        :param master: Parent widget
+        :param placeholder: Placeholder text to put in the widget
+        :type placeholder: str
+        :param color: color of the placeholder text
+        :type color: str
+        :param **kwargs: keyword arguments passed on to ttk::Entry
+        :type kwargs: dict
+        """
         super().__init__(master, **kwargs)
 
         self.placeholder = placeholder
         self.placeholder_color = color
         self.style = ttk.Style(self)
 
-        self.bind("<FocusIn>", self.foc_in)
-        self.bind("<FocusOut>", self.foc_out)
+        self.bind("<FocusIn>", self._on_focus_in)
+        self.bind("<FocusOut>", self._on_focus_out)
 
-        self.put_placeholder()
+        self._put_placeholder()
         self["style"] = "Custom.TEntry"
 
-    def put_placeholder(self):
+    def _put_placeholder(self):
+        """ Puts the placeholder text in the entry, and sets the foreground color of the style to the proper color"""
         self.insert(0, self.placeholder)
         self.style.configure("Custom.TEntry", foreground=self.placeholder_color)
 
-    def foc_in(self, *args):
+    def _on_focus_in(self, *args):
         if self.style.lookup("Custom.TEntry", 'foreground') == self.placeholder_color:
             self.delete('0', 'end')
             self.style.configure("Custom.TEntry", foreground=self.style.lookup("TEntry", "foreground"))
 
-    def foc_out(self, *args):
+    def _on_focus_out(self, *args):
         if not self.get():
-            self.put_placeholder()
+            self._put_placeholder()
