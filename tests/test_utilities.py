cbea5d982d7b7982a17c4581c333cd0822622304
RedFantom <redfantom@outlook.com>
2019-12-14 12:33:57 +0100

    Extend tests for move_widget for widgets with bindings

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -7,15 +7,23 @@ from tkinter import ttk
 
 
 class TestUtilities(BaseWidgetTest):
+    def setUp(self):
+        BaseWidgetTest.setUp(self)
+        self._dummy_flag = False
+
     def assertIsChild(self, child, parent):
         self.assertIn(child, parent.children.values())
         parent_of_parent = parent.winfo_parent()
         if not parent_of_parent.endswith("."):
             parent_of_parent += "."
         self.assertEquals(child.winfo_parent(), parent_of_parent + parent.winfo_name())
+
+    def assertHasBeenInvoked(self):
+        self.assertTrue(self._dummy_flag)
+        self._dummy_flag = False
     
-    def _dummy_bind(self, event):
-        pass
+    def _dummy_bind(self, _=None):
+        self._dummy_flag = True
     
     def test_move_widget(self):
         label = ttk.Label(self.window)
@@ -60,16 +68,57 @@ class TestUtilities(BaseWidgetTest):
         self.assertIsChild(label, tl)
         self.assertIn('<Enter>', label.bind())
 
+    def test_move_widget_with_command(self):
+        widget = ttk.Button(self.window, command=self._dummy_bind)
+        self.assertIsChild(widget, self.window)
+        widget.invoke()
+        self.assertHasBeenInvoked()
+
+        parent = tk.Toplevel()
+        child = move_widget(widget, parent)
+        self.assertIsChild(child, parent)
+        widget.invoke()
+        self.assertHasBeenInvoked()
+
+    def test_move_widget_with_bound_method_on_parent(self):
+        tl1 = tk.Toplevel(self.window)
+        tl2 = tk.Toplevel(self.window)
+        tl1._dummy_bind = self._dummy_bind
+
+        button = ttk.Button(tl1)
+        button.bind("<Enter>", tl1._dummy_bind)
+        button.event_generate("<Enter>")
+        self.assertHasBeenInvoked()
+
+        button = move_widget(button, tl2)
+        button.event_generate("<Enter>")
+        self.assertHasBeenInvoked()
+
+    def test_move_widget_with_command_method_on_parent(self):
+        tl1 = tk.Toplevel(self.window)
+        tl2 = tk.Toplevel(self.window)
+        tl1._dummy_bind = self._dummy_bind
+
+        button = ttk.Button(tl1, command=tl1._dummy_bind)
+        button.invoke()
+        self.assertHasBeenInvoked()
+
+        button = move_widget(button, tl2)
+        button.invoke()
+        self.assertHasBeenInvoked()
+
     def test_move_widget_with_binding_on_parent(self):
-        self.window.bind('<Enter>', self._dummy_bind)
-        label = ttk.Label(self.window)
-        label.pack()
-        tl = tk.Toplevel(self.window)
-        label = move_widget(label, tl)
-        label.pack()
-        self.assertTrue(label.winfo_parent() == tl.winfo_parent() + '.' + tl.winfo_name())
-        self.assertIn('<Enter>', tl.bind())
-    
+        widget = ttk.Label(self.window)
+        widget._dummy_bind = self._dummy_bind
+        self.window.bind("<Enter>", widget._dummy_bind)
+
+        self.window.event_generate("<Enter>")
+        self.assertHasBeenInvoked()
+
+        move_widget(widget, tk.Toplevel())
+        self.window.event_generate("<Enter>")
+        self.assertHasBeenInvoked()
+
     def test_parse_geometry(self):
         g = parse_geometry('1x1+1+1')
         self.assertEqual(g, (1, 1, 1, 1))
