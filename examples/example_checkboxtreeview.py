ea43c9ebc32bafcb258e0167a4465b0d33148390
Juliette Monsel <j_4321@protonmail.com>
2020-02-04 11:53:30 +0100

    Add disabled item in CheckboxTreeview example

--- examples/example_checkboxtreeview.py
+++ examples/example_checkboxtreeview.py
@@ -12,8 +12,8 @@ tree = CheckboxTreeview(root)
 tree.pack()
 
 tree.insert("", "end", "1", text="1")
-tree.insert("1", "end", "11", text="11")
-tree.insert("1", "end", "12",  text="12")
+tree.insert("1", "end", "11", text="11 (disabled)", tags='disabled')
+tree.insert("1", "end", "12", text="12")
 tree.insert("11", "end", "111", text="111")
 tree.insert("", "end", "2", text="2")
 
