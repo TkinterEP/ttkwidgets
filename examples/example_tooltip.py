084bf132217884c35763d315da1ce8f1a79274eb
Malek <malek.cellier@gmail.com>
2021-01-31 10:22:19 +0100

    Update example_tooltip.py
    It seems the name of that feature has been changed from Tooltip to Balloon but the example was not updated.

--- examples/example_tooltip.py
+++ examples/example_tooltip.py
@@ -2,12 +2,12 @@
 
 # Copyright (c) RedFantom 2017
 # For license see LICENSE
-from ttkwidgets.frames import Tooltip
+from ttkwidgets.frames import Balloon
 import tkinter as tk
 
 
 window = tk.Tk()
 button = tk.Button(window, text="Button", command=window.destroy)
 button.pack()
-balloon = Tooltip(button)
+balloon = Balloon(button)
 window.mainloop()
