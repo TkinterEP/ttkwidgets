3bad128adf27058e68eb690873839578e913d206
rdbende <rdbende@gmail.com>
2021-03-25 15:56:48 +0100

    Update test for NumberEntry

--- tests/test_numberentry.py
+++ tests/test_numberentry.py
@@ -8,25 +8,23 @@ import tkinter as tk
 
 class TestNumberEntry(BaseWidgetTest):
     def test_numberentry_init(self):
-        entry = NumberEntry(self.window, expressions=True, roundto=4)
+        entry = NumberEntry(self.window, roundto=4, allowed_chars={'p': 3.14})
         entry.pack()
         self.window.update()
 
     def test_numberentry_events(self):
-        entry = NumberEntry(self.window, expressions=True, roundto=4)
+        entry = NumberEntry(self.window, roundto=4, allowed_chars={'p': 3.14})
         entry.pack()
         self.window.update()
-        entry.insert(0, "1+2-3*4/5**6")
+        entry.insert(0, "1+2-3*4/5**p")
         self.window.update()
         entry._check()
-        self.window.update()
-        entry._replace("1+2-3*4/5**6")
         self.window.update()
         entry._eval()
         self.window.update()
 
     def test_numberentry_config(self):
-        entry = NumberEntry(self.window, expressions=True, roundto=4)
+        entry = NumberEntry(self.window, roundto=4, allowed_chars={'p': 3.14})
         entry.pack()
         self.window.update()
         entry.keys()
@@ -37,5 +35,5 @@ class TestNumberEntry(BaseWidgetTest):
         self.window.update()
         value = entry["roundto"]
         self.window.update()
-        entry["roundto"] = 4
+        entry["allowed_chars"] = {'p': 3.14159}
         self.window.update()
