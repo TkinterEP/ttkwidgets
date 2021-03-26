e80986d72287793157825bed408559ee7514d590
rdbende <rdbende@gmail.com>
2021-03-26 16:41:28 +0100

    ToggledFrame improved
    Since Tk 8.6 supports png, I don't think it's actually necessary to use PIL. I hope no one uses Tk 8.5 these days.
    I know I forgot something very important but I can't remember what it is.

--- ttkwidgets/frames/toggledframe.py
+++ ttkwidgets/frames/toggledframe.py
@@ -2,11 +2,13 @@
 Author: RedFantom
 License: GNU GPLv3
 Source: This repository
+
+Improved by rdbende
 """
+
 import tkinter as tk
 from tkinter import ttk
 import os
-from PIL import Image, ImageTk
 from ttkwidgets.utilities import get_assets_directory
 
 
@@ -16,8 +18,8 @@ class ToggledFrame(ttk.Frame):
 
     :ivar interior: :class:`ttk.Frame` in which to put the widgets to be toggled with any geometry manager.
     """
-
-    def __init__(self, master=None, text="", width=20, compound=tk.LEFT, **kwargs):
+    
+    def __init__(self, master=None, **kwargs):
         """
         Create a ToggledFrame.
 
@@ -28,33 +30,87 @@ class ToggledFrame(ttk.Frame):
         :param width: width of the closed ToggledFrame (in characters)
         :type width: int
         :param compound: "center", "none", "top", "bottom", "right" or "left":
-                         position of the toggle arrow compared to the text
+            position of the toggle arrow compared to the text
         :type compound: str
+        :param opened: whether the frame should be opened by default
+        :type opened: bool
+        :param cursor: cursor that appears on the toggler-checkbutton
+        :type cursor: str
         :param kwargs: keyword arguments passed on to the :class:`ttk.Frame` initializer
         """
+        self._compound = kwargs.pop("compound", tk.LEFT)
+        self._cursor = kwargs.pop("cursor", "arrow")
+        self._open = kwargs.pop("opened", False)
+        self._text = kwargs.pop("text", None)
+        self._width = kwargs.pop("width", 20)
+        self._toggled = tk.BooleanVar(value=self._open)
         ttk.Frame.__init__(self, master, **kwargs)
-        self._open = False
-        self.__checkbutton_var = tk.BooleanVar()
-        self._open_image = ImageTk.PhotoImage(Image.open(os.path.join(get_assets_directory(), "open.png")))
-        self._closed_image = ImageTk.PhotoImage(Image.open(os.path.join(get_assets_directory(), "closed.png")))
-        self._checkbutton = ttk.Checkbutton(self, style="Toolbutton", command=self.toggle,
-                                            variable=self.__checkbutton_var, text=text, compound=compound,
-                                            image=self._closed_image, width=width)
-        self.interior = ttk.Frame(self, relief=tk.SUNKEN)
-        self._grid_widgets()
+        
+        self._open_image = tk.PhotoImage(file=os.path.join(get_assets_directory(), "open.png"))
+        self._closed_image = tk.PhotoImage(file=os.path.join(get_assets_directory(), "closed.png"))
+        self._button = ttk.Checkbutton(self, style="Toolbutton", image=self._closed_image,
+                                       cursor=self._cursor, variable=self._toggled,
+                                       text=self._text, command=self.toggle,
+                                       compound=self._compound, width=self._width)
+        self._button.grid(row=0, column=0, sticky="ew")
+        self.interior = ttk.Frame(self)
+        if self._open:
+            self.toggle()
+    
+    def __getitem__(self, key):
+        return self.cget(key)
 
-    def _grid_widgets(self):
-        self._checkbutton.grid(row=0, column=0, sticky="we")
+    def __setitem__(self, key, value):
+        self.configure(**{key: value})
 
-    def toggle(self):
+    def toggle(self, *args):
         """Toggle :obj:`ToggledFrame.interior` opened or closed."""
         if self._open:
             self._open = False
-            self.__checkbutton_var.set(False)
+            self._toggled.set(False)
             self.interior.grid_forget()
-            self._checkbutton.config(image=self._closed_image)
+            self._button.config(image=self._closed_image)
+            self.event_generate("<<ToggledFrameClosed>>")
         else:
             self._open = True
-            self.__checkbutton_var.set(True)
+            self._toggled.set(True)
             self.interior.grid(row=1, column=0, sticky="nswe")
-            self._checkbutton.config(image=self._open_image)
+            self._button.config(image=self._open_image)
+            self.event_generate("<<ToggledFrameOpened>>")
+        self.event_generate("<<ToggledFrameToggled>>")
+            
+    def configure(self, **kwargs):
+        """Configure resources of the widget"""
+        self._compound = kwargs.pop("compound", self._compound)
+        self._cursor = kwargs.pop("cursor", self._cursor)
+        self._open = kwargs.pop("opened", self._open)
+        self._text = kwargs.pop("text", self._text)
+        self._width = kwargs.pop("width", self._width)
+        self._button.configure(text=self._text, cursor=self._cursor, compound=self._compound, width=self._width)
+        ttk.Frame.configure(self, **kwargs)
+        self._open = not self._open
+        self.toggle()
+        
+    config = configure
+            
+    def cget(self, key):
+        """Return the resource value for a KEY given as string"""
+        if key == "compound":
+            return self._compound
+        elif key == "cursor":
+            return self._cursor
+        elif key == "opened":
+            return self._opened
+        elif key == "text":
+            return self._text
+        elif key == "width":
+            return self._width
+        else:
+            return ttk.Frame.cget(key)
+    
+    def keys(self):
+        """Return a list of all resource names of this widget"""
+        keys = ttk.Frame.keys()
+        keys.extend(["compound", "cursor", "opened", "text", "width"])
+        keys.sort()
+        return keys
