364021d4a57e506db6199c7c44c868a0ef535d39
rdbende <rdbende@gmail.com>
2022-12-13 20:09:47 +0100

    Update LinkLabel tests

--- tests/test_linklabel.py
+++ tests/test_linklabel.py
@@ -1,30 +1,31 @@
 # Copyright (c) RedFantom 2017
 # For license see LICENSE
-from ttkwidgets import LinkLabel
-from tests import BaseWidgetTest
 import tkinter as tk
+
+from tests import BaseWidgetTest
+from ttkwidgets import LinkLabel
 
 
 class TestLinkLabel(BaseWidgetTest):
     def test_linklabel_init(self):
         label = LinkLabel(self.window, link="www.google.com", text="Visit Google")
         label.pack()
-        self.window.update()
 
     def test_linklabel_events(self):
         label = LinkLabel(self.window, link="www.google.com", text="Visit Google")
         label.pack()
+
         self.window.update()
         label._on_enter()
         self.window.update()
         label._on_leave()
         self.window.update()
         label.open_link()
-        self.window.update()
 
     def test_linklabel_config(self):
         label = LinkLabel(self.window, link="www.google.com", text="Visit Google")
         label.pack()
+
         self.window.update()
         label.keys()
         self.window.update()
@@ -36,12 +37,16 @@ class TestLinkLabel(BaseWidgetTest):
         self.window.update()
         label["clicked_color"] = "purple"
         self.window.update()
+        label.config(cursor="hand1")
+        self.window.update()
+        assert label.cget("cursor") == "hand1"
 
     def test_linklabel_cget(self):
         label = LinkLabel(self.window, link="www.google.com", text="Visit Google")
         label.pack()
-        assert label.cget("hover_color") == label._hover_color
+
         assert label.cget("link") == label._link
         assert label.cget("normal_color") == label._normal_color
+        assert label.cget("hover_color") == label._hover_color
         assert label.cget("clicked_color") == label._clicked_color
         assert label.cget("text") == "Visit Google"
